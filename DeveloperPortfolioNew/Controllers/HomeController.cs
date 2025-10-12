baf1cfb5babc5a7cc62e96ae119769674aa6a8d9
Evan <evan@ix.co.za>
2025-10-12 11:44:57 +0200

    Adding code to fetch list of frameworks from api

--- DeveloperPortfolioNew/Controllers/HomeController.cs
+++ DeveloperPortfolioNew/Controllers/HomeController.cs
@@ -7,14 +7,34 @@ namespace DeveloperPortfolioNew.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly IHttpClientFactory _httpClientFactory;
+		private readonly string _apiUrl = "https://localhost:7207/api";
+		private readonly string _getFrameworks = "/";
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
 		{
 			_logger = logger;
+			_httpClientFactory = httpClientFactory;
 		}
 
-		public IActionResult Index()
+		public async Task<IActionResult> Index()
 		{
+			// Fetch list of frameworks
+			HttpClient client = _httpClientFactory.CreateClient();
+			HttpResponseMessage response = new HttpResponseMessage();
+
+			try
+			{
+				response = await client.GetAsync(_apiUrl + _getFrameworks);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+
+			List<Framework> frameworks = new List<Framework>();
+
+			
 			return View();
 		}
 
---
+++ DeveloperPortfolioNew/Models/Framework.cs
@@ -0,0 +1,9 @@
+﻿namespace DeveloperPortfolioNew.Models
+{
+	public class Framework
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public bool IsDisplay { get; set; }
+	}
+}
